bfca81bb32e240ff3e223bd24ff726c8b617c03a
Billy Hao <billyhao12@gmail.com>
2020-05-03 17:44:16 -0700

    User can now save tasks using localStorage

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // Displays the current date at the top of the page
 $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
+// Sets a variable for all save buttons
+var saveBtn = $('.save');
+
+// Retrieves previously saved tasks from localStorage and stores them in the tasks object
+var tasks = JSON.parse(localStorage.getItem('tasks')) || {};
+
+// Populates text fields with previously saved text
+saveBtn.each(function() {
+    var buttonHour = $(this).data('hour');
+    var taskInput = $('#task-' + buttonHour);
+
+    taskInput.val( tasks['task' + buttonHour]);
+});
+
+// Saves user's input into local storage upon clicking a save button
+saveBtn.on('click', function (event) {
+
+    var buttonHour = $(this).data('hour');
+    var taskInput = $('#task-' + buttonHour);
+    var taskValue = taskInput.val();
+
+    tasks['task' + buttonHour] = taskValue;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+});
